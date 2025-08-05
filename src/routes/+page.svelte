4e409f1884a76a8c801749beea31362404366a1d
freedom5566 <YongXuanJ0815@gmail.com>
2025-08-05 20:57:08 +0800

    add daisyuid

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,5 @@
 <h1>Welcome to SvelteKit</h1>
+<button class="btn btn-primary">One</button>
+<button class="btn btn-secondary">Two</button>
+<button class="btn btn-accent btn-outline">Three</button>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
