e6c7b60ff110b1da29db3dc14ed2140b1a586c3d
freedom5566 <YongXuanJ0815@gmail.com>
2025-08-09 22:56:59 +0800

    add env val

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import favicon from '$lib/assets/favicon.svg';
 	import '../app.css';
+	const baseUrl = __BASE_URL__;
 	let { children } = $props();
 </script>
 
@@ -29,28 +30,28 @@
 			<ul
 				class="menu menu-sm dropdown-content bg-neutral text-neutral-content rounded-box z-1 mt-3 w-52 p-2 shadow"
 			>
-				<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">首頁</a></li>
-				<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">怪物掉落查詢</a></li>
-				<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">物品掉落查詢</a></li>
+				<li><a class="btn btn-ghost text-xl" href={baseUrl}>首頁</a></li>
+				<li><a class="btn btn-ghost text-xl" href={baseUrl}>怪物掉落查詢</a></li>
+				<li><a class="btn btn-ghost text-xl" href={baseUrl}>物品掉落查詢</a></li>
 
-				<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">常見問題</a></li>
-				<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1/about">關於我們</a></li>
+				<li><a class="btn btn-ghost text-xl" href={baseUrl}>常見問題</a></li>
+				<li><a class="btn btn-ghost text-xl" href="{baseUrl}/about">關於我們</a></li>
 			</ul>
 		</div>
-		<a class="btn btn-ghost text-xl" href="https://127.0.0.1">Problytic</a>
+		<a class="btn btn-ghost text-xl" href={baseUrl}>Problytic</a>
 	</div>
 	<div class="navbar-center hidden lg:flex">
 		<ul class="menu menu-horizontal px-1">
-			<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">首頁</a></li>
-			<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">怪物掉落查詢</a></li>
-			<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">物品掉落查詢</a></li>
+			<li><a class="btn btn-ghost text-xl" href={baseUrl}>首頁</a></li>
+			<li><a class="btn btn-ghost text-xl" href={baseUrl}>怪物掉落查詢</a></li>
+			<li><a class="btn btn-ghost text-xl" href={baseUrl}>物品掉落查詢</a></li>
 
-			<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">常見問題</a></li>
-			<li><a class="btn btn-ghost text-xl" href="https://127.0.0.1">關於我們</a></li>
+			<li><a class="btn btn-ghost text-xl" href={baseUrl}>常見問題</a></li>
+			<li><a class="btn btn-ghost text-xl" href={baseUrl}>關於我們</a></li>
 		</ul>
 	</div>
 	<div class="navbar-end">
-		<a class="btn btn-primary" href="https://127.0.0.1">登入</a>
+		<a class="btn btn-primary" href={baseUrl}>登入</a>
 	</div>
 </div>
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 
-export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+// export default defineConfig({
+// 	plugins: [tailwindcss(), sveltekit()],
+// 	server: {
+//     	host: '0.0.0.0',
+//     	port: 5173
+//   }
+// });
+
+export default defineConfig(({ mode }) => {
+	const env = loadEnv(mode, process.cwd(), '');
+
+	return {
+		plugins: [tailwindcss(), sveltekit()],
+		server: {
+			host: '0.0.0.0',
+			port: 5173
+		},
+		define: {
+			__BASE_URL__: JSON.stringify(env.VITE_BASE_URL)
+		}
+	};
 });
